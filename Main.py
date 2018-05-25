4cd6b9af0e1be49af01d0f293950622076a2107e
rootick <bryxin.dmitr@gmail.com>
2018-05-25 03:35:04 +0300

    1

--- Main.py
+++ Main.py
@@ -3,12 +3,13 @@
 import time
 import sqlite3
 import telebot
+import os
+from flask import Flask, request
 
-
-BOT_TOKEN = '486750815:AAFQ2XOflRepZFeLJuac-0Ii3vM61ToDg6o'
+TOKEN = '486750815:AAFQ2XOflRepZFeLJuac-0Ii3vM61ToDg6o'
 CHANNEL_NAME = '-1001275366258'
 
-bot = telebot.TeleBot(BOT_TOKEN)
+bot = telebot.TeleBot(TOKEN)
 
 
 def new_post_key():
@@ -38,7 +39,18 @@ def new_post_key():
 
 
 
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
 
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://botbtc.herokuapp.com/' + TOKEN)
+    return "!", 200
 
 if __name__ == '__main__':
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
     new_post_key()
