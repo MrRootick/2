25f30c240a8fa854ff565c741aeb5b521754f562
rootick <bryxin.dmitr@gmail.com>
2018-05-25 03:36:46 +0300

    1

--- Main.py
+++ Main.py
@@ -48,7 +48,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://botbtc.herokuapp.com/' + TOKEN)
+    bot.set_webhook(url='https://chanbo.herokuapp.com/' + TOKEN)
     return "!", 200
 
 if __name__ == '__main__':
